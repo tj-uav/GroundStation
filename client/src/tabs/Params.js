a05556a3ff7e7860c1c9a654ae14e598948d87bc
Srikar Gouru <srikarg89@gmail.com>
2020-04-06 00:19:31 -0400

    Finish params search

--- client/src/tabs/Params.js
+++ client/src/tabs/Params.js
@@ -1,15 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ButtonGroup from 'react-bootstrap/ButtonGroup'
 import Button from 'react-bootstrap/Button'
 import Form from 'react-bootstrap/Form'
 import Table from 'react-bootstrap/Table'
 
+const params = [
+    ["Bank angle", "30", "The plane's maximum bank angle"],
+    ["RC Failsafe", "RTL", "What to do when RC connection is lost"],
+    ["Geofence", "RTL", "What to do when plane exits geofence"]
+]
 
 const Params = () => {
 
+    const [display, setDisplay] = useState(params);
+
+    const search = (event) => {
+        let form = event.currentTarget;
+        let input = form.childNodes[0].value;
+        console.log(input); // Search based on this value
+        let temp = [];
+        for(let i in params){
+            let param = params[i];
+            if(param[0].toLowerCase().includes(input.toLowerCase())){
+                temp.push(param);
+            }
+        }
+        setDisplay(temp);
+    }
+
     return (
         <div>
-            <div style={{"float": "left", "padding-left": 20}}>
+            <div style={{"float": "left", "paddingLeft": 20}}>
                 <Table striped bordered hover>
                     <thead>
                         <tr>
@@ -19,28 +40,20 @@ const Params = () => {
                         </tr>
                     </thead>
                     <tbody>
-                        <tr>
-                            <td>Bank Angle</td>
-                            <td>30</td>
-                            <td>The plane's maximum bang angle</td>
+                        {display.map((element, i) => 
+                        <tr key={i}>
+                            <td>{element[0]}</td>
+                            <td>{element[1]}</td>
+                            <td>{element[2]}</td>
                         </tr>
-                        <tr>
-                            <td>RC Failsafe</td>
-                            <td>RTL</td>
-                            <td>What to do when RC connection is lost</td>
-                        </tr>
-                        <tr>
-                            <td>Geofence</td>
-                            <td>RTL</td>
-                            <td>What to do when plane exists geofence</td>
-                        </tr>
+                        )}
                     </tbody>
                 </Table>
             </div>
-            <ButtonGroup vertical style={{"float": "right", "padding-right": 30}}>
+            <ButtonGroup vertical style={{"float": "right", "paddingRight": 30}}>
                 <Button variant="primary">Read params</Button>
                 <Button variant="primary">Write params</Button>
-            <Form style={{"margin-top": 20}} inline={true}>
+            <Form style={{"marginTop": 20}} inline={true} onSubmit={(event) => search(event)}>
                 <Form.Control type="text" placeholder="Search params"></Form.Control>
                 <Button variant="primary" type="submit">Search params</Button>
             </Form>
