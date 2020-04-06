7b09c53586142c4a7bd20d38c9d05bd271ae69ac
Srikar Gouru <srikarg89@gmail.com>
2020-04-05 23:31:39 -0400

    Initialize react app

---
+++ client/src/tabs/Data.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Data = () => {
+
+    return (
+        <div>You opened the data tab</div>
+    )
+}
+
+export default Data;
---
+++ client/src/tabs/FlightPlan.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const FlightPlan = () => {
+
+    return (
+        <div>You opened the flight plan tab</div>
+    )
+}
+
+export default FlightPlan;
