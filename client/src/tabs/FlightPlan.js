1bc69eb052e10421b795f54de4452a23cc0e828c
Srikar Gouru <srikarg89@gmail.com>
2020-04-08 21:21:05 -0400

    Create multiple polygons

--- client/src/tabs/FlightPlan.js
+++ client/src/tabs/FlightPlan.js
@@ -4,14 +4,15 @@ import FlightPlanToolbar from '../components/FlightPlanToolbar.js'
 import SplitPane from 'react-split-pane'
 //import InputPage from '../components/test.js'
 
-// TODO: Popup for home icon
-// TODO: Popup for waypoint number (maybe use different icon for this)
-// TODO: Add option to make new polygon
+// TODO: Home icon
+// TODO: Waypoint number icon
 // TODO: Implement marker insertion
-// TODO: Implement marker removal
 // TODO: Display current location of plane (use telem, and also need to make plane icon)
 // TODO: Polyline overlay -> take polyline file (custom file structure) and overlay it onto map (allow for color option in file)
-// TODO: Commands
+// TODO: Commands display in toolbar
+// TODO: Commands creation in toolbar
+// TODO: Interactive display list (move around, delete, insert)
+// TODO: Fix error where waypoint and fence modes display polygon points
 
 
 const FlightPlan = (props) => {
@@ -19,7 +20,7 @@ const FlightPlan = (props) => {
     const [mode, setMode] = useState("waypoints")
     const [waypoints, setWaypoints] = useState([]);
     const [commands, setCommands] = useState([]);
-    const [polygons, setPolygons] = useState([]);
+    const [polygons, setPolygons] = useState([[]]);
     const [fence, setFence] = useState([]);
 
     const getters = {
