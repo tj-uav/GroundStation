208a4a5126b8f136f6464145d48e892555106e5f
Srikar Gouru <srikarg89@gmail.com>
2020-04-09 13:29:05 -0400

    Dynamic arrows

--- client/src/tabs/FlightPlan.js
+++ client/src/tabs/FlightPlan.js
@@ -3,17 +3,19 @@ import FlightPlanMap from '../components/FlightPlanMap.js'
 import FlightPlanToolbar from '../components/FlightPlanToolbar.js'
 import SplitPane from 'react-split-pane'
 
-// TODO: Home icon
-// TODO: Waypoint number icon
-// TODO: Implement marker insertion
-// TODO: Display current location of plane (use telem, and also need to make plane icon)
-// TODO: Polyline overlay -> take polyline file (custom file structure) and overlay it onto map (allow for color option in file)
-// TODO: Commands display in toolbar
-// TODO: Commands creation in toolbar
-// TODO: Interactive display list (move around, delete, insert)
-// TODO: Fix error where waypoint and fence modes display polygon points
-// TODO: Polyline arrows showing direction of waypoints
-// TODO: Display list highlighting
+/*
+TODO: Home icon
+TODO: Waypoint number icon
+TODO: Implement marker insertion
+TODO: Display current location of plane (use telem, and also need to make plane icon)
+TODO: Polyline overlay -> take polyline file (custom file structure) and overlay it onto map (allow for color option in file)
+TODO: Commands display in toolbar
+TODO: Commands creation in toolbar
+TODO: Interactive display list (move around, delete, insert)
+TODO: Fix error where waypoint and fence modes display polygon points
+T̶O̶D̶O̶:̶ P̶o̶l̶y̶l̶i̶n̶e̶ a̶r̶r̶o̶w̶s̶ s̶h̶o̶w̶i̶n̶g̶ d̶i̶r̶e̶c̶t̶i̶o̶n̶ o̶f̶ w̶a̶y̶p̶o̶i̶n̶t̶s̶
+TODO: Display list highlighting (and vice versa)
+*/
 
 
 const FlightPlan = (props) => {
