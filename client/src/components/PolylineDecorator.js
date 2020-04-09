d24f67f502e5fc68adf2eb49af9da22246ae37d1
Srikar Gouru <srikarg89@gmail.com>
2020-04-09 13:00:48 -0400

    Waypoint polylines have arrows

---
+++ client/src/components/PolylineDecorator.js
@@ -0,0 +1,42 @@
+import React, { useRef, useEffect } from "react";
+import { Polyline, withLeaflet } from "react-leaflet";
+import L from "leaflet";
+import "leaflet-polylinedecorator";
+
+
+const PolylineDecorator = withLeaflet(props => {
+
+  const arrow = [
+    {
+      offset: "50%",
+      repeat: 0,
+      symbol: L.Symbol.arrowHead({
+        pixelSize: 20,
+        polygon: false,
+        pathOptions: { stroke: true, color: props.color }
+      })
+    }
+  ];  
+
+  const polyRef = useRef();
+  useEffect(() => {
+    const polyline = polyRef.current.leafletElement; //get native Leaflet polyline
+    const { map } = polyRef.current.props.leaflet; //get native Leaflet map
+
+    let temp = [];
+    let latlngs = polyline.getLatLngs();
+    if(latlngs.length > 1){
+      for(let i of Array(latlngs.length - 1).keys()){
+        temp.push([latlngs[i], latlngs[i+1]]);
+      }
+    }
+
+    L.polylineDecorator(temp, {
+        patterns : arrow
+    }).addTo(map);
+  });
+
+  return <Polyline ref={polyRef} patterns={arrow} {...props} />;
+});
+
+export default PolylineDecorator;
