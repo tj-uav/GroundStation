080457e021ebdaeed28f69abbb57ab096853d7ce
Jklein64 <jklein.64@verizon.net>
2020-06-24 15:33:02 -0400

    Demonstrate use of Button components

--- client/src/tabs/FlightData.js
+++ client/src/tabs/FlightData.js
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from 'react';
+import { Button } from "../components/Button"
+import { Row, Column } from "../components/Containers"
 import { httpget } from '../backend.js'
 
 const FlightData = () => {
@@ -17,6 +19,16 @@ const FlightData = () => {
 
   return (
     <div>You opened the flight data tab {telem[0]} {telem[1]} {telem[2]}
+      <Row gap="1rem" height="3rem" width="40rem">
+        <Button>Click Me!</Button>
+        <Button>Click Me!</Button>
+        <Button>Click Me!</Button>
+      </Row>
+      <Column gap="1rem" height="11rem" width="20rem" style={{ marginTop: "200px" }}>
+        <Button>Click Me!</Button>
+        <Button>Click Me!</Button>
+        <Button>Click Me!</Button>
+      </Column>
     </div>
   )
 }
