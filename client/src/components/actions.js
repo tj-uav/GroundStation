f003e6de64af9ffd8eaf9caa3adc862fad0142e1
PyroBorg <ronithreddy7@gmail.com>
2020-06-29 18:04:26 -0400

    Update to actions tab in flight data

--- client/src/components/actions.js
+++ client/src/components/actions.js
@@ -27,31 +27,46 @@ const Actions = props => {
                 <Button>Servo</Button>
             </Row>
             <br /><br />
-            <Row gap="1rem" height="3rem" width="40rem">
-                <Column gap="1rem" height="30rem" width="10rem">
+            <Row gap="1rem" height="11rem" width="40rem">
+                <Column gap="1rem" height="10rem" width="10rem">
                     <Button>Actions</Button>
                     <Button>Waypoint</Button>
                     <Button>Flight</Button>
                     <Button>Mount</Button>
                 </Column>
-                <Column gap="1rem" height="30rem" width="10rem">
+                <Column gap="1rem" height="10rem" width="10rem">
                     <Button>Do Action</Button>
                     <Button>Set Waypoint</Button>
                     <Button>Set Mode</Button>
                     <Button>Set Mount</Button>
                 </Column>
-                <Column gap="1rem" height="30rem" width="10rem">
+                <Column gap="1rem" height="10rem" width="10rem">
                     <Button>Auto</Button>
                     <Button>Loiter</Button>
                     <Button>RTL</Button>
                     <Button>Clear Track</Button>
                 </Column>
-                <Column gap="1rem" height="30rem" width="10rem">
+                <Column gap="1rem" height="10rem" width="10rem">
                     <Button>Set Home Alt</Button>
                     <Button>Restart Mission</Button>
                     <Button>Raw View</Button>
                     <Button>Arm/Disarm</Button>
                 </Column>
+            </Row>
+            <Row gap="1rem" height="11rem" width="20rem">
+                <Column gap="1rem" height="10rem" width="10rem">
+                    <Box label="Speed" content={speed} />
+                    <Box label="Altitude" content={altitude} />
+                    <Box label="Loiter Rate" content={loiterRate} />
+                </Column>
+                <Column gap="1rem" height="10rem" width="15rem">
+                    <Box />
+                    <Box />
+                    <Box />
+                </Column>
+            </Row>
+            <Row gap="1rem" height="11rem" width="25rem">
+                <Box label="Console/Error Messages" />
             </Row>
         </div>
     );
