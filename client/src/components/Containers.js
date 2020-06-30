6c4926ef36ccc593e2c203028efb3e2f3f06b46d
Jklein64 <jklein.64@verizon.net>
2020-06-30 15:12:49 -0400

    Add flexibility

--- client/src/components/Containers.js
+++ client/src/components/Containers.js
@@ -22,18 +22,18 @@ import styled from "styled-components"
 
 export const Row = styled.div`
     display: grid;
-    grid-template-rows: 1fr;
+    grid-template-rows: ${props => props.height ?? "1fr"};
     gap: ${props => props.gap ?? "1rem"};
     width: ${props => props.width ?? "100%"};
     height: ${props => props.height ?? "unset"};
-    grid-template-columns: repeat(auto-fit, minmax(0, 1fr));
+    grid-template-columns: ${props => props.columns ?? "repeat(auto-fit, minmax(0, 1fr))"};
 `
 
 export const Column = styled.div`
     display: grid;
     grid-template-columns: 1fr;
     gap: ${props => props.gap ?? "1rem"};
-    width: ${props => props.width ?? "10rem"};
+    width: ${props => props.width ?? "100%"};
     height: ${props => props.height ?? "unset"};
-    grid-template-rows: repeat(auto-fit, minmax(0, 1fr));
+    grid-template-rows: repeat(auto-fit, minmax(0, ${props => props.height ?? "1fr"}));
 `
