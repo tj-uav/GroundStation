1f57a250d7439acedda10e792d4cac5ac2199b65
Aryan34 <44515360+Aryan34@users.noreply.github.com>
2020-07-03 01:00:33 -0400

    Start view and submitted tab for submissions page

---
+++ client/src/components/submissions/tabs/Submitted.js
@@ -0,0 +1,72 @@
+import React, { useState, useEffect } from "react"
+import { Button, Box, Label } from "../../UIElements"
+import { Row, Column } from "../../Containers"
+
+const SubmittedRow = ({ shape, shapeColor, letter, letterColor, orientation, field, ...props }) => {
+	return (
+		<Column gap="1rem">
+			<Row height="4rem">
+				<Box content={shape} label="Shape" line="200%" />
+				<Box content={letter} label="Letter" line="200%" />
+				<Box content={orientation} label="Orientation" line="200%" />
+			</Row>
+			<Row height="4rem">
+				<Box content={shapeColor} label="Shape Color" line="200%" />
+				<Box content={letterColor} label="Letter Color" line="200%" />
+				<Box content={field} label="Field" line="200%" />
+			</Row>
+		</Column>
+	)
+}
+
+const Submitted = props => {
+	// const [altitude, setAltitude] = useState(0)
+	// const [orientation, setOrientation] = useState(0)
+	// const [groundSpeed, setGroundSpeed] = useState(0)
+	// const [airspeed, setAirspeed] = useState(0)
+	// const [text, setText] = useState(0)
+	// const [battery, setBattery] = useState(0)
+	// const [throttle, setThrottle] = useState(0)
+	// const [latLong, setLatLong] = useState(0)
+
+	// const updateData = () => {
+	// 	setAltitude(Math.floor(Math.random() * 200) + 100)
+	// 	setOrientation(Math.floor(Math.random() * 360))
+	// 	setGroundSpeed(Math.floor(Math.random() * 50) + 25)
+	// 	setAirspeed(Math.floor(Math.random() * 50) + 25)
+	// 	setText("N/A")
+	// 	setBattery(Math.floor(Math.random() * 100))
+	// 	setThrottle(Math.floor(Math.random() * 100))
+	// 	setLatLong([Math.floor(Math.random() * 360), Math.floor(Math.random() * 360)])
+	// }
+
+	// useEffect(() => {
+	// 	const tick = setInterval(() => {
+	// 		updateData()
+	// 	}, 250)
+	// 	return () => clearInterval(tick)
+	// })
+
+	return (
+		<div
+			style={{
+				display: "flex",
+				flexDirection: "column",
+				height: "calc(100vh - 9.5rem)",
+			}}
+		>
+			<Column gap="2rem" style={{overflowY: "auto"}}>
+				<SubmittedRow shape={"Square"} shapeColor={"Blue"} letter={"A"} letterColor={"Red"} orientation={420} field={"SUBMITTED: IDK wut this is"} />
+				<SubmittedRow shape={"Square"} shapeColor={"Blue"} letter={"A"} letterColor={"Red"} orientation={420} field={"SUBMITTED: IDK wut this is"} />
+				<SubmittedRow shape={"Square"} shapeColor={"Blue"} letter={"A"} letterColor={"Red"} orientation={420} field={"SUBMITTED: IDK wut this is"} />
+				{/* <SubmittedRow shape={"Square"} shapeColor={"Blue"} letter={"A"} letterColor={"Red"} orientation={420} field={"IDK wut this is"} />
+				<SubmittedRow shape={"Square"} shapeColor={"Blue"} letter={"A"} letterColor={"Red"} orientation={420} field={"IDK wut this is"} />
+				<SubmittedRow shape={"Square"} shapeColor={"Blue"} letter={"A"} letterColor={"Red"} orientation={420} field={"IDK wut this is"} />
+				<SubmittedRow shape={"Square"} shapeColor={"Blue"} letter={"A"} letterColor={"Red"} orientation={420} field={"IDK wut this is"} />
+				<SubmittedRow shape={"Square"} shapeColor={"Blue"} letter={"A"} letterColor={"Red"} orientation={420} field={"IDK wut this is"} /> */}
+			</Column>
+		</div>
+	)
+}
+
+export default Submitted
--- client/src/pages/Submissions.js
+++ client/src/pages/Submissions.js
@@ -1,28 +1,77 @@
 import React, { useState, useEffect } from "react"
 import SubmissionsToolbar from "../components/submissions/SubmissionsToolbar.js"
+import View from "../components/submissions/tabs/View.js"
+import Submitted from "../components/submissions/tabs/Submitted.js"
+import { Row } from "../components/Containers"
+import { Button } from "../components/UIElements"
 import axios from "axios"
+
+const Tabs = ({ ...props }) => {
+	const [tab, setTab] = useState(<View />)
+	const [active, setActive] = useState(tab.type.name)
+
+	const modifyActive = isActive => {
+		setActive(isActive)
+	}
+
+	const viewButton = (
+		<Button
+			onClick={() => {
+				setTab(<View />)
+				modifyActive(View.name)
+			}}
+			active={active === View.name}
+			controlled
+		>
+			{View.name}
+		</Button>
+	)
+
+	const submittedButton = (
+		<Button
+			onClick={() => {
+				setTab(<Submitted />)
+				modifyActive(Submitted.name)
+			}}
+			active={active === Submitted.name}
+			controlled
+		>
+			{Submitted.name}
+		</Button>
+	)
+
+	return (
+		<section>
+			<Row id="tabs" gap="1rem" height="3rem" style={{ marginBottom: "1rem" }} {...props}>
+				{viewButton}
+				{submittedButton}
+			</Row>
+			{tab}
+		</section>
+	)
+}
 
 const Submissions = () => {
 	const [submissions, setSubmissions] = useState([])
 
-	const get = async (endpoint, func) => {
-		axios
-			.get(endpoint, {
-				headers: { "Content-Type": "application/json", Accept: "application/json" },
-			})
-			.then(function (response) {
-				func(response)
-			})
-	}
+	// const get = async (endpoint, func) => {
+	// 	axios
+	// 		.get(endpoint, {
+	// 			headers: { "Content-Type": "application/json", Accept: "application/json" },
+	// 		})
+	// 		.then(function (response) {
+	// 			func(response)
+	// 		})
+	// }
 
-	useEffect(() => {
-		const interval = setInterval(() => {
-			get("/odlcs/all/all", response => {
-				console.log(response)
-			})
-		}, 1000)
-		return () => clearInterval(interval)
-	}, [])
+	// useEffect(() => {
+	// 	const interval = setInterval(() => {
+	// 		get("/odlcs/all/all", response => {
+	// 			console.log(response)
+	// 		})
+	// 	}, 1000)
+	// 	return () => clearInterval(interval)
+	// }, [])
 
 	return (
 		<div
@@ -36,6 +85,7 @@ const Submissions = () => {
 				overflowY: "hidden",
 			}}
 		>
+			<Tabs />
 			<SubmissionsToolbar/>
 		</div>
 	)
