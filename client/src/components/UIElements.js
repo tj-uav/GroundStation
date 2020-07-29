e39b68335c7da30d1998259610a6583d6957a79d
Jklein64 <jklein.64@verizon.net>
2020-07-29 14:50:41 -0400

    Create Dropdown component

--- client/src/components/UIElements.js
+++ client/src/components/UIElements.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react"
 import { dark, darker, blue, red } from "../theme/Colors"
 import styled from "styled-components"
+/* eslint import/no-webpack-loader-syntax: off */
+import { ReactComponent as Caret } from "../icons/caret.svg"
 
 export const Button = ({ active, controlled, ...props }) => {
 	const [isActive, setActive] = useState(active ?? false)
@@ -54,6 +56,84 @@ const StyledButton = styled.a`
 		bottom: 0;
 		height: 0.5rem;
 		background: ${blue};
+	}
+`
+
+export const Dropdown = ({ children, onOptionSet = undefined, ...props }) => {
+	const [active, setActive] = useState(false)
+	const [option, setOptionRaw] = useState(undefined)
+	const setOption = onOptionSet ?? setOptionRaw
+	let options = children.map(c => c.props.children)
+	options.sort() // native is in-place, idk why
+
+	return (
+		<div style={{ position: "relative" }} onMouseLeave={() => setActive(false)}>
+			<StyledDropdown
+				className="paragraph"
+				active={active}
+				onClick={() => setActive(!active)}
+				{...props}
+			>
+				{option ?? "--"}
+				<StyledCaret width={16} active={active} />
+			</StyledDropdown>
+			{active ? (
+				options.map((option, i) => (
+					<DropdownContent
+						key={i}
+						number={i}
+						setOption={ID => {
+							setOption(ID)
+							setActive(false)
+						}}
+					>
+						{option}
+					</DropdownContent>
+				))
+			) : (
+				<></>
+			)}
+		</div>
+	)
+}
+
+const StyledCaret = styled(Caret)`
+	transform: ${props => (props.active ? "scaleY(-1)" : "scaleY(1)")};
+	transition: transform 0.1s ease;
+`
+
+const StyledDropdown = styled(StyledButton)`
+	padding: 0 1rem;
+	justify-content: space-between;
+	height: 100%;
+`
+
+const DropdownContent = ({ children, setOption, number, ...props }) => {
+	const ID = useState(children)[0]
+	return (
+		<StyledDropdownContent id={ID} number={number} onClick={() => setOption(ID)} {...props}>
+			{children}
+		</StyledDropdownContent>
+	)
+}
+
+const StyledDropdownContent = styled(StyledButton)`
+	transform: translateY(${props => props.number * 100}%);
+	justify-content: flex-start;
+	color: black !important;
+	position: absolute;
+	padding-left: 1rem;
+	cursor: pointer;
+	height: 2rem;
+	width: 100%;
+	z-index: 3;
+
+	::after {
+		display: none;
+	}
+
+	&:hover {
+		color: ${blue} !important;
 	}
 `
 
