abdefd0cb11333c3f1507fbfe11b5514e76dccbc
Jklein64 <jklein.64@verizon.net>
2020-10-30 16:08:31 -0400

    Use intersectionObserver to make params lagless

--- client/src/components/params/param/index.js
+++ client/src/components/params/param/index.js
@@ -3,11 +3,11 @@ import React, { useState } from "react"
 import Active from "./Active"
 import Normal from "./Normal"
 
-export default ({ data }) => {
+export default ({ data, ...props }) => {
 	const [active, setActive] = useState(false)
 
 	// prettier-ignore
 	return active
-	    ? <Active data={data} hook={[active, setActive]} />
-	    : <Normal data={data} hook={[active, setActive]} />
+	    ? <Active {...props} data={data} hook={[active, setActive]} />
+	    : <Normal {...props} data={data} hook={[active, setActive]} />
 }
