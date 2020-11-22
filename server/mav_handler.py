b62f7713dffb2aa52b2fbb5b202f7fa4f4d64dc2
gnanduru1 <ganesh.nanduru@gmail.com>
2020-11-22 12:00:50 -0500

    geofence cmd

--- server/mav_handler.py
+++ server/mav_handler.py
@@ -5,8 +5,9 @@ BAUDRATE = 115200
 
 COMMANDS = {
     "TAKEOFF": mavutil.mavlink.MAV_CMD_NAV_TAKEOFF,
-    "WAYPOINT": mavutil.mavlink.MAV_CMD_NAV_WAYPOINT
-    "LAND": mavutil.mavlink.MAV_CMD_NAV_LAND
+    "WAYPOINT": mavutil.mavlink.MAV_CMD_NAV_WAYPOINT,
+    "LAND": mavutil.mavlink.MAV_CMD_NAV_LAND,
+    "GEOFENCE": MAV_CMD_NAV_FENCE_POLYGON_VERTEX_INCLUSION
 }
 
 class MavHandler:
