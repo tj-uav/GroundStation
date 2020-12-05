386430be1b7649abce867bedea2dc95fee1cfa2d
Aryan34 <44515360+Aryan34@users.noreply.github.com>
2020-12-05 11:14:10 -0500

    SocketIO stuff

--- server/main.py
+++ server/main.py
@@ -1,49 +1,59 @@
+import random
 from flask import Flask, jsonify, request
 from flask_cors import CORS
+from flask_socketio import SocketIO, send, emit
+
 # from interop_handler import InteropHandler
-import logging
-log = logging.getLogger('werkzeug')
-log.setLevel(logging.ERROR)
+# import logging
+# log = logging.getLogger('werkzeug')
+# log.setLevel(logging.ERROR)
 
 app = Flask(__name__)
 CORS(app)
+socketio = SocketIO(app, cors_allowed_origins="*")
+
 
 # interop = InteropHandler(1)
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+# @app.route("/")
+# def hello():
+#     return "Hello World!"
 
 
-@app.route("/interop/login", methods=["GET", "POST"])
-def interop_login():
-    if request.method == "POST":
-        try:
-            data = request.get_json()
-            interop.login(data['ip'], data['username'], data['password'])
-        except:
-            return jsonify({"status": False})
-    return jsonify({"status": interop.login_status})
+# @app.route("/interop/login", methods=["GET", "POST"])
+# def interop_login():
+#     if request.method == "POST":
+#         try:
+#             data = request.get_json()
+#             interop.login(data['ip'], data['username'], data['password'])
+#         except:
+#             return jsonify({"status": False})
+#     return jsonify({"status": interop.login_status})
+#
+#
+# @app.route("/interop/get/<key>")
+# def interop_get(key):
+#     return jsonify(interop.get_data(key))
+#
+#
+# @app.route("/interop/odlcs/<id>/<dtype>")
+# def odcl_get(id, dtype):
+#     return jsonify(interop.get_odlcs(id, dtype))
 
 
-@app.route("/interop/get/<key>")
-def interop_get(key):
-    return jsonify(interop.get_data(key))
+@socketio.on("connect")
+def connect():
+    print("Connected")
+    return
 
 
-@app.route("/interop/odlcs/<id>/<dtype>")
-def odcl_get(id, dtype):
-    return jsonify(interop.get_odlcs(id, dtype))
-
-
-@app.route("/mav/telem")
+@socketio.on("message")
 def telem():
-    import random
-    return jsonify([random.randint(0, 50), random.randint(0, 50), random.randint(0, 50), 
-        random.randint(0, 50), random.randint(0, 50), random.randint(0, 50), 
-        random.randint(0, 50), random.randint(0, 50),
-    ])
-
+    print("DATA")
+    data = {'altitude': random.randint(0, 50), 'orientation': random.randint(0, 50),
+            'groundspeed': random.randint(0, 50), 'airspeed': random.randint(0, 50), 'text': random.randint(0, 50),
+            'battery': random.randint(0, 50), 'throttle': random.randint(0, 50), 'latlong': random.randint(0, 50)}
+    emit('responseMessage', data)
 
 if __name__ == "__main__":
-    app.run(port=5000, debug=False)
+    socketio.run(app)
