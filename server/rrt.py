4adb42472975b9b01cddec3ff64bbed3d1c3d2d0
PyroBorg <ronithreddy7@gmail.com>
2021-01-27 15:10:30 -0500

    -rrt algorithm update -added obstacle implementation -obstacles currently use (x, y) coords; not geo coords

--- server/rrt.py
+++ server/rrt.py
@@ -35,6 +35,25 @@ dTheta = pi/12
 MIN_PHI = 0
 MAX_PHI = pi
 
+class Obstacle():
+    # obstacle at x,y with radius r
+    def __init__(self, lat, lon, z, r):
+        self.lat = lat
+        self.lon = lon
+        self.z = z
+        self.r = r
+
+    # for debugging
+    def __str__(self):
+        return "Center: ({}. {}), Radius: {}".format(round(self.lat, 3), round(self.lon, 3), round(self.r, 3))
+
+    def inMe(self, lat, lon, z):
+        global radius_tolerance, height_tolerance
+        return dist((self.lat, self.lon), (lat, lon))<=(self.r+radius_tolerance) and z<=(self.z+height_tolerance)
+    
+    def KMLFriendly(self):
+        return [self.lat, self.lon, self.z]
+
 @functools.total_ordering
 class Node():
     def __init__(self, lat, lon, z=100, parent=None, theta=0, phi=pi/2):
@@ -82,13 +101,13 @@ def dist(p1, p2):
     return sqrt((p1[0]-p2[0])*(p1[0]-p2[0])+(p1[1]-p2[1])*(p1[1]-p2[1]))
 
 def great_circle_conv(lat, lon, dN, dE):
-   earth_r = 6378137
-   dLat = dN/earth_r
-   dLon = dE/(earth_r*cos(pi*lat/180))
-   return (lat+dLat*180/pi, lon + dLon * 180/pi)
+    earth_r = 6378137
+    dLat = dN/earth_r
+    dLon = dE/(earth_r*cos(pi*lat/180))
+    return (lat+dLat*180/pi, lon + dLon * 180/pi)
 
 def great_circle_dist(lat1, lon1, lat2, lon2):
-   return distance.great_circle((lat1,lon1),(lat2,lon2)).meters
+    return distance.great_circle((lat1,lon1),(lat2,lon2)).meters
 
 def step_from_to(p1,p2):
     if p1.dist(p2) < EPSILON:
@@ -125,12 +144,16 @@ def main():
 
     nodes = []
     start = (38.147250, -76.426444)
-    end = great_circle_conv(start[0], start[1], 220, -140)
     goalNode = Node(100, 100)
-    
+    # obs_coord = great_circle_conv(start[0], start[1], 50, -50)
+    # print(obs_coord)
+    # obs = Obstacle(obs_coord[0], obs_coord[1], 100, 30)
 
     nodes.append(Node(YDIM / 2.0, XDIM / 2.0)) # Start in the center
 #    nodes.append((0.0,0.0)) # Start in the corner
+    obs = Obstacle(YDIM / 2.0 - 50, XDIM / 2.0 - 50, 100, 30)
+
+    pygame.draw.circle(screen, white, (XDIM / 2.0 - 50, YDIM / 2.0 - 50), 30)
 
     i = 0
     while i < NUMNODES:
@@ -143,24 +166,18 @@ def main():
                     if rand.dist(p) < rand.dist(nn):
                         nn = p
         newnode = step_from_to(nn,rand)
-        if nn.parent == None:
-            nodes.append(newnode)
-            newnode.parent = nn
-            pygame.draw.line(screen,white,(nn.lon, nn.lat),(newnode.lon, newnode.lat))
-            pygame.display.update()
-        #        print i, "    ", nodes
-            if goalNode.dist(newnode) < EPSILON:
-                goalNode.parent = newnode
-                break
-        elif abs(newnode.getHeading() - nn.getHeading()) < MAX_RELATIVE_BANK:
-            nodes.append(newnode)
-            newnode.parent = nn
-            pygame.draw.line(screen,white,(nn.lon, nn.lat),(newnode.lon, newnode.lat))
-            pygame.display.update()
-        #        print i, "    ", nodes
-            if goalNode.dist(newnode) < EPSILON:
-                goalNode.parent = newnode
-                break
+        new_loc = newnode.loc()
+        # print(obs.inMe(new_loc[0], new_loc[1], 100))
+        if not obs.inMe(new_loc[0], new_loc[1], 100):
+            if nn.parent == None or abs(newnode.getHeading() - nn.getHeading()) < MAX_RELATIVE_BANK:
+                nodes.append(newnode)
+                newnode.parent = nn
+                pygame.draw.line(screen,white,(nn.lon, nn.lat),(newnode.lon, newnode.lat))
+                pygame.display.update()
+            #        print i, "    ", nodes
+                if goalNode.dist(newnode) < EPSILON:
+                    goalNode.parent = newnode
+                    break
         i += 1
     
     path = [(goalNode.lat, goalNode.lon)]
