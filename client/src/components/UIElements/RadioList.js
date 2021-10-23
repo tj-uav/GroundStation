933b458c4ffa7a4b746427a53893f28253714a8c
william blakc <williamblack7586@gmail.com>
2021-10-23 12:25:35 -0400

    Add ability to change custom radio button color

--- client/src/components/UIElements/RadioList.js
+++ client/src/components/UIElements/RadioList.js
@@ -1,6 +1,5 @@
 import React from "react"
 import styled from "styled-components"
-import "./RadioList.css"
 
 const RadioList = props => {
     let children = props.children.map(c => React.cloneElement(c, { name: props.name }));
@@ -14,7 +13,7 @@ const RadioList = props => {
 const Option = props => {
     return (
         <StyledOption>
-            <input className="radio" styles="background: purple;" type="radio" id={props.id ?? props.value} value={props.value} name={props.name} />
+            <StyledRadio color={props.color} type="radio" id={props.id ?? props.value} value={props.value} name={props.name} />
             {props.children}
         </StyledOption>
     )
@@ -23,6 +22,24 @@ const Option = props => {
 const StyledOption = styled.div`
     vertical-align: middle;
     font-family: Poppins, sans-serif;
+`
+
+const StyledRadio = styled.input`
+    margin-right: 0.4rem;
+    margin-left: 0.4rem;
+    appearance: none;
+    height: 1rem;
+    width: 1rem;
+    border-radius: 50%;
+    padding: 0;
+    background: #f1ebe5;
+    border: 2px solid #5a5755;
+    vertical-align: -2px;
+
+    &:checked {
+        border: 4px solid ${props => props.color ?? "#629dee"};
+        transition: 0.1s;
+    }
 `
 
 RadioList.Option = Option
