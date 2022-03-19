3d586fd1b5c7d3c3b10a844c0eb05750b018ac03
Krishnan Shankar <krishnans2006@gmail.com>
2022-03-19 12:51:56 -0400

    Fix UAV Mission Actions, Add API Routes and Action Buttons for UGV

--- server/app/ugv/commands.py
+++ server/app/ugv/commands.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, request, current_app as app
+from flask import Blueprint, request, current_app as app, send_file
 
 from errors import InvalidRequestError
 
@@ -21,6 +21,29 @@ def ugv_insert_command():
                        f.get("lon"),
                        f.get("alt")
                        )
+
+
+@ugv_commands.route("/jump", methods=["POST"])
+def uav_jump_command():
+    f = request.json
+    if not all(field in f for field in ["command"]):
+        raise InvalidRequestError("Missing required fields in request")
+    return app.gs.call("uav_jumpcommand", f.get("command"))
+
+
+@ugv_commands.route("/save", methods=["POST"])
+def uav_save_commands():
+    return app.gs.call("uav_savecommands")
+
+
+@ugv_commands.route("/load", methods=["POST"])
+def uav_load_commands():
+    return app.gs.call("uav_loadcommands")
+
+
+@ugv_commands.route("/view")
+def uav_view_commands_file():
+    return send_file("handlers/pixhawk/uav/uav_mission.txt")
 
 
 @ugv_commands.route("/clear", methods=["POST"])
