c53aa6447f5d96aeba75f2642dc1cfa3128f5dde
william blakc <williamblack7586@gmail.com>
2022-06-12 16:32:37 -0400

    Change Map to Satellite Map

--- client/src/components/FlightMap.js
+++ client/src/components/FlightMap.js
@@ -110,7 +110,7 @@ const FlightPlanMap = props => {
 			fetch("https://g.co", {
 				mode: "no-cors"
 			}).then(() => {
-				tileRef.current.setUrl("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png")
+				tileRef.current.setUrl("https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}")
 			}).catch(() => {
 				tileRef.current.setUrl("/map/{z}/{x}/{y}.png")
 			})
@@ -260,7 +260,7 @@ const FlightPlanMap = props => {
 				style={{ height: "100%" }}
 			>
 				<TileLayer
-					attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+					attribution="Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community"
 					url={"/map/{z}/{x}/{y}.png"}
 					ref={tileRef}
 				/>
