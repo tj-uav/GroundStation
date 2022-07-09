6318df19ec0e7b2790359b2dc61b25b0072c0484
Krishnan Shankar <krishnans2006@gmail.com>
2022-07-09 19:16:18 -0400

    Black refactor of backend

--- client/public/assets/change_color.py
+++ client/public/assets/change_color.py
@@ -2,6 +2,6 @@ import cv2
 
 img = cv2.imread("icon-fence.png", cv2.IMREAD_UNCHANGED)
 dst = img.copy()
-dst[:,:,0] = img[:,:,2]
-dst[:,:,2] = img[:,:,0]
+dst[:, :, 0] = img[:, :, 2]
+dst[:, :, 2] = img[:, :, 0]
 cv2.imwrite("icon-polygons.png", dst)
