dde3eaacea038d75a30c52d60666a593e21a40a6
TJUAV <tjhsstuav@gmail.com>
2023-06-17 11:54:42 -0400

    Updated "." to represent "/dev/ttyUSB0"
    Co-authored-by: Krishnan Shankar <krishnans2006@gmail.com>
    Co-authored-by: Joshua <joshuapikner@gmail.com>

--- util/run-gs.sh
+++ util/run-gs.sh
@@ -5,12 +5,15 @@ source "$HOME"/.zshrc  # to support nvm
 SCRIPT_DIR=${0:a:h}
 
 port_input=""
-vared -p "Enter port override (enter \".\" for default): " port_input
+vared -p "Enter port override (enter \".\" for ttyUSB0): " port_input
 
 # Set port config in server/config.json
 if [ "$port_input" != "." ]
 then
     port=$(printf '%s\n' "$port_input" | sed -e 's/[\/&]/\\&/g')  # escaped for sed
+    sed -i "s/\"port\": \".*\",/\"port\": \"$port\",/" "$SCRIPT_DIR"/../server/config.json
+else
+    port="\/dev\/ttyUSB0"
     sed -i "s/\"port\": \".*\",/\"port\": \"$port\",/" "$SCRIPT_DIR"/../server/config.json
 fi
 
