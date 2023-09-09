a3ac0d30700a9e50b90fc861a981b81852ca1a11
william black <williamblack7586@gmail.com>
2023-09-09 11:15:57 -0400

    Fix active parameter size display

--- client/src/pages/Params/Param/Active.js
+++ client/src/pages/Params/Param/Active.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react"
+import React, { useEffect, useState } from "react"
 
 import { Row, Column } from "components/Containers"
 import { Label } from "components/UIElements"
@@ -9,27 +9,32 @@ import Submit from "./Submit"
 
 import { useParameters } from "../../Params"
 
-const Active = ({ listRef, style, height, data, index, setActiveIndex, setModifiedIndexes, parametersSave }) => {
+const Active = ({ listRef, style, data, index, setActiveIndex, setModifiedIndexes, setActiveSize, parametersSave }) => {
 	const [value, setValue] = useState(data.value)
 	const deactivate = () => setActiveIndex(-1)
 	const [params, setParameters] = useParameters()
+	const root = React.useRef()
+
+	useEffect(() => {
+		setActiveSize(root.current.getBoundingClientRect().height)
+	}, [])
 
 	return (
 		<form onSubmit={e => handleSubmit(e, deactivate)}>
-			<Row style={{ ...style, height: height }} columns="min-content auto 6rem">
-				<div style={{ display: "flex", flexDirection: "column", gap: "1rem" }}>
+			<Row style={{ ...style, height: undefined }} columns="min-content auto 6rem">
+				<div style={{ display: "flex", flexDirection: "column", gap: "1rem", height: "95%" }}>
 					<Row height="2rem" columns="11.5rem 5rem">
 						<Content padded children={data.name} />
 						<Value editable hook={[value, setValue]} />
 					</Row>
-					<Row style={{ marginBottom: "-1rem" }}>
+					<Row style={{ marginBottom: "-1.15rem" }}>
 						<Label>Options</Label>
 					</Row>
 					<Row>
 						<Content padded children={data.options ?? "no options defined."} />
 					</Row>
 				</div>
-				<Column style={{ display: "flex", height: "96%" }}>
+				<Column ref={root} style={{ display: "flex", paddingBottom: "2px" }}>
 					<Content>
 						<b>{data.DisplayName}</b>{data.DisplayName ? ". " : ""}{data.Description}
 					</Content>
@@ -39,7 +44,7 @@ const Active = ({ listRef, style, height, data, index, setActiveIndex, setModifi
 						display: "flex",
 						flexDirection: "column",
 						rowGap: "1rem",
-						height: "96%",
+						paddingBottom: "2px"
 					}}
 				>
 					<Submit
